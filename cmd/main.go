a43112c873380439aa8768fc93be997244f7a92e
Lalitchitoshiya <luckeychitoshiya@gmail.com>
2026-01-08 08:46:38 +0000

    Added support for search survior, locs using phone

--- cmd/main.go
+++ cmd/main.go
@@ -1,20 +1,29 @@
 package main
 
 import (
-	 "github.com/gin-gonic/gin"
-	 "github.com/illusioniststg/hopeline/internal/planning"
+	"github.com/gin-gonic/gin"
+	"github.com/illusioniststg/hopeline/internal/data"
+	"github.com/illusioniststg/hopeline/internal/identity"
+	"github.com/illusioniststg/hopeline/internal/planning"
 )
 
 func pong(c *gin.Context) {
-    c.JSON(200, gin.H{
-      "message": "pong from server",
-    })
-  }
+	c.JSON(200, gin.H{
+		"message": "pong from server",
+	})
+}
 
 func main() {
+	data.ReadSurvivorsFromCSV("internal/data/basedata.csv")
 	router := gin.Default()
 	router.GET("/ping", pong)
+
+	router.GET("/data/survivors", data.GetSurvivors)
 	// http://localhost:8080/assign?survivor_id=123&boat_id=boatA
 	router.GET("/assign", planning.AssignBoat)
+	// http://localhost:8080/search
+	router.POST("/search", identity.SearchPerson)
+	// http://localhost:8080/search
+	router.POST("/locs", identity.GetLocs)
 	router.Run() // listens on 0.0.0.0:8080 by default
 }
--- internal/domain/survivor.go
+++ internal/domain/survivor.go
@@ -1,15 +1,21 @@
 package domain
 
 type Survivor struct {
-    ID          int64      `json:"id" db:"id"`
-    FirstName   string     `json:"first_name" db:"first_name"`
-    LastName    string     `json:"last_name" db:"last_name"`
-    DOB         string     `json:"dob" db:"dob"`
-    Phone       string     `json:"phone" db:"phone"`
-    Locs        []Location `json:"locs" db:"locs"`
-    IsInjured   bool       `json:"is_injured" db:"is_injured"`
-    IsMissing   bool       `json:"is_missing" db:"is_missing"`
-    ShelterID   int64      `json:"shelter_id" db:"shelter_id"`
-    BoatID      int64      `json:"boat_id" db:"boat_id"`
-    UpdatedAt   string     `json:"updated_at" db:"updated_at"`
+	FirstName string     `json:"first_name" db:"first_name"`
+	LastName  string     `json:"last_name" db:"last_name"`
+	DOB       string     `json:"dob" db:"dob"`
+	Gender    string     `json:"gender" db:"gender"`
+	Phone     string     `json:"phone" db:"phone"`
+	Locs      []Location `json:"locs" db:"locs"`
+	Village   string     `json:"village" db:"village"`
+	District  string     `json:"district" db:"district"`
+	State     string     `json:"state" db:"state"`
+	IsInjured bool       `json:"is_injured" db:"is_injured"`
+	IsMissing bool       `json:"is_missing" db:"is_missing"`
+	ShelterID int64      `json:"shelter_id" db:"shelter_id"`
+	BoatID    int64      `json:"boat_id" db:"boat_id"`
+}
+
+type SurvivorSearchRequest struct {
+	Phone string `json:"phone"`
 }
